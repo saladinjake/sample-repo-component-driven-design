92642529c211f7c2d6057c9df2c40e27ae9613a3
juwavictor <victor@sloovi.com>
2024-09-18 15:31:02 +0100

    chore: add util function

--- src/utilities/helperFuncs.ts
+++ src/utilities/helperFuncs.ts
@@ -1,1 +1,14 @@
-export default {}
+import { css } from 'styled-components'
+export const trimString = (value: string, maxStringLength: number): string => {
+  if (!value) return ''
+  return value.length > maxStringLength
+    ? `${value.substring(0, maxStringLength)}...`
+    : value
+}
+
+export const verifyInputNumberOnly = e => {
+  if (/^\D/.test(e.key) && e.key.length === 1) {
+    e.preventDefault()
+    return
+  }
+}
