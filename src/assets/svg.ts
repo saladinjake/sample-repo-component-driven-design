6f6d88899280a5c7959df24b47bbba9ef9136ca5
juwavictor <victor@sloovi.com>
2024-09-19 19:12:27 +0100

    chore: add svg and animation

---
+++ src/assets/svg.ts
@@ -0,0 +1,41 @@
+import Search from "./svg/search.svg";
+import ChevronRight from "./svg/chevron-right.svg";
+import Error from "./svg/error.svg";
+import Info from "./svg/info.svg";
+import SuccessAnimation from "./svg/success-animation.svg";
+import ErrorAnimation from "./svg/error-animation.svg";
+import LineArrowDown from "./svg/line-arrow-down.svg";
+import KudaNerve from "./svg/kuda-nerve.svg";
+import CogWheel from "./svg/cog-wheel.svg";
+import Upload from "./svg/upload.svg";
+import CancelCircle from "./svg/cancel-circle.svg";
+import Cancel from "./svg/cancel.svg";
+import SuccessCircle from "./svg/Clipped.svg";
+import RefreshCircle from "./svg/Refresh.svg";
+import Calendar from "./svg/calendar.svg";
+import RedStar from "./svg/redstar.svg";
+import CircleAvatar from "./svg/Circle_Avatar.svg";
+import CheckMark from "./svg/checkmark.svg";
+import Failed from "./svg/failed.svg";
+
+export const Svg = {
+  Calendar,
+  Cancel,
+  CancelCircle,
+  CheckMark,
+  ChevronRight,
+  CircleAvatar,
+  CogWheel,
+  Error,
+  ErrorAnimation,
+  Failed,
+  Info,
+  KudaNerve,
+  LineArrowDown,
+  RedStar,
+  RefreshCircle,
+  Search,
+  SuccessAnimation,
+  SuccessCircle,
+  Upload,
+};
